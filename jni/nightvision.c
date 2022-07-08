667f82255d5bdead01c699fe95b54b752817a5b7
ExpressLens <expresslens@proton.me>
2022-07-08 17:54:57 +0000

    link updates

--- jni/nightvision.c
+++ jni/nightvision.c
@@ -3,4 +3,8 @@
 
 /* Private: Convert a single ARGB pixel to grayscale (i.e. 0 - 255). */
 uint8_t rgbToGrayscale(argb* pixel) {
-    return 0.3 * pixel->red + 0.59 * p
+    return 0.3 * pixel->red + 0.59 * pixel->green + 0.11 * pixel->blue;
+}
+
+jboolean Java_com_ford_openxc_nightvision_NightVisionView_detectObjects(
+        JNIEnv* 
