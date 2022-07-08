4cd03e4eaadae0137bebb4a8f0ccb63cbb437552
ExpressLens <expresslens@proton.me>
2022-07-08 17:00:30 +0000

    Release 1.9.0

--- jni/nightvision.c
+++ jni/nightvision.c
@@ -1,4 +1,6 @@
 #include "nightvision.h"
 #include <stdbool.h>
 
-/* Private: Convert a single ARGB pixel to grayscale 
+/* Private: Convert a single ARGB pixel to grayscale (i.e. 0 - 255). */
+uint8_t rgbToGrayscale(argb* pixel) {
+    return 0.3 * pixel->red + 0.59 * p
