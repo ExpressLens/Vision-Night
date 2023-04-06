8b62fabf52be2ba58144b4900e65b1473a3db372
ExpressLens <expresslens@proton.me>
2023-04-06 08:40:18 +0000

    project config

--- jni/nightvision.h
+++ jni/nightvision.h
@@ -5,4 +5,8 @@
 
 #define LOG_TAG "NightVisionJNI"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+#define OBJECT_DETECT_BLOCK_SIZE_X 8
+#define OBJECT_DETECT_BLOCK_SIZE_Y 8
+#define OBJ
