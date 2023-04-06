3775282b18a4f3156488e3990e67b1c7df3168fd
ExpressLens <expresslens@proton.me>
2023-04-06 06:45:01 +0000

    USB: serial: add support for iBall 3.5G connect usb modem

--- jni/nightvision.h
+++ jni/nightvision.h
@@ -4,4 +4,5 @@
 #include <stdlib.h>
 
 #define LOG_TAG "NightVisionJNI"
-#define LOGI(.
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__
